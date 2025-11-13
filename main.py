00c07c747b5940b4331684055afad51b26625e4b
Andres Alonso <andres@cyberinsights.es>
2025-11-13 13:02:11 +0100

    add duckdb data to gitignore

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import tools.qdb as qdb
 
 # Initialize the database
 qdb.setup_database(qdb.duckdb_conn)
+
+# Navigation
 home = st.Page("pages/0_home.py", title="Home", icon="🏠")
 document = st.Page("pages/1_document.py", title="Documentation", icon="📄")
 request = st.Page("pages/2_request.py", title="Create a request", icon="📝")
